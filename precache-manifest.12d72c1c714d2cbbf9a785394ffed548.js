207e3bb09e5482a2cd3bef96a55c09eb21383376
Kay <pnkhoa2609@gmail.com>
2019-12-04 22:20:22 +0700

    Updates

---
+++ precache-manifest.12d72c1c714d2cbbf9a785394ffed548.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d44b2f6ffb7ddc331d64fc0d48395db",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "0661415651c6fac35e70",
+    "url": "/dashboard/static/css/main.6878ada3.chunk.css"
+  },
+  {
+    "revision": "a9738b867cec729f9e43",
+    "url": "/dashboard/static/js/2.7aefc1c6.chunk.js"
+  },
+  {
+    "revision": "0661415651c6fac35e70",
+    "url": "/dashboard/static/js/main.b4c7653a.chunk.js"
+  },
+  {
+    "revision": "c3b6194f8fbaaa3c359b",
+    "url": "/dashboard/static/js/runtime-main.33484abf.js"
+  }
+]);
